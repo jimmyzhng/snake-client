e587350ca23ae89364382b222e5b3d3f256b2a80
Jimmy <jimmyzhang1@hotmail.com>
2022-11-23 18:15:12 -0800

    Refactored code, added input file for handling user input

---
+++ input.js
@@ -0,0 +1,21 @@
+// setup interface to handle user input from stdin
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  // This runs handleUserInput when input is received from keyboard
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+module.exports = setupInput;
