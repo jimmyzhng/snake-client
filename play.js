ca36fd077069941daa9065e12a0a89b2c505ed6c
Jimmy <jimmyzhang1@hotmail.com>
2022-11-23 17:24:00 -0800

    added function that handle's users input. ctrl+c exits from the game

--- play.js
+++ play.js
@@ -1,5 +1,27 @@
 const net = require('net');
+const { stderr, stdin } = require('process');
 const connect = require('./client');
+
+// setup interface to handle user input from stdin
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  // This runs handleUserInput when input is received from keyboard
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
 
 console.log("Connecting..");
 connect();
+setupInput();
